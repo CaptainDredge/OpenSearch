171c6d9af2c12cbaffb7ce174b65ccbff0cc44db
Prabhat Sharma <ptsharma@amazon.com>
2024-05-09 11:11:16 +0530

    Added range benchmark for routing

--- benchmarks/src/main/java/org/opensearch/benchmark/routing/allocation/DocRoutingBenchmark.java
+++ benchmarks/src/main/java/org/opensearch/benchmark/routing/allocation/DocRoutingBenchmark.java
@@ -29,6 +29,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.TreeSet;
 
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @SuppressWarnings("unused")
@@ -168,13 +169,6 @@ public class DocRoutingBenchmark {
 
         void routeDoc(Document doc) {
             final int shardId = route(doc.getDocId(), true);
-//            if(inMemoryFileStore.containsKey(shardId)) {
-//                inMemoryFileStore.get(shardId).add(doc);
-//            } else {
-//                List<Document> docs = new ArrayList<>();
-//                docs.add(doc);
-//                inMemoryFileStore.put(shardId, docs);
-//            }
         }
 
         Document getDoc(int docId) {
@@ -190,13 +184,6 @@ public class DocRoutingBenchmark {
 
         private Partition findParent(int shardId) throws Exception {
             int maxShardNum = docs.size() - 1;
-//            for (int i = 0; i < routingTable.size(); ++i) {
-//                for (int j = 0; j < routingTable.get(i).size(); ++j) {
-//                    if (routingTable.get(i).get(j) == shardId) {
-//                        return metaData.get(i);
-//                    }
-//                }
-//            }
 
             for(int i=0; i<=maxShardNum; i++) {
                 for(int j=0; j<2; j++) {
@@ -214,9 +201,168 @@ public class DocRoutingBenchmark {
         }
     }
 
+    private static class IndexWithRangeRouting {
+        private final List<Integer> docs = new ArrayList<>(0);
+        private final HashMap<Integer, List<Document>> inMemoryFileStore = new HashMap<>();
+        private final Map<Integer, TreeSet<HashRange>> primaryShardRanges = new HashMap<>();
+        private final Map<Integer, SplitShardRange> shardRanges = new HashMap<>();
+        private final List<List<Integer>> routingTable = new ArrayList<>(0);
+        private final int numPrimaryShard;
+        private final int routingFactor;
+        private final int routingShard;
+
+        IndexWithRangeRouting(int numPrimaryShard) {
+            this.numPrimaryShard = numPrimaryShard;
+            this.routingShard = calculateNumRoutingShards(this.numPrimaryShard);
+            this.routingFactor = this.routingShard / this.numPrimaryShard;
+            for(int i = 0; i < numPrimaryShard; ++i) {
+                docs.add(0);
+                routingTable.add(new ArrayList<>(0));
+                this.primaryShardRanges.put(i, new TreeSet<>(List.of(new HashRange(0, i))));
+            }
+        }
+
+        void Split(final int shardId, int numShards) {
+            List<HashRange> splitRanges;
+            int primaryShardId;
+            if(shardId < numPrimaryShard) {
+                primaryShardId = shardId;
+                TreeSet<HashRange> ranges = primaryShardRanges.get(shardId);
+                splitRanges = ranges.first().splitRange(numShards, docs.size() - 1);
+                ranges.clear();
+                ranges.addAll(splitRanges);
+            } else {
+                SplitShardRange splitShardRange = shardRanges.get(shardId);
+                primaryShardId = splitShardRange.primaryShardId;
+                TreeSet<HashRange> ranges = primaryShardRanges.get(primaryShardId);
+                ranges.remove(splitShardRange.range);
+                splitRanges = splitShardRange.range.splitRange(numShards, docs.size() - 1);
+                ranges.addAll(splitRanges);
+            }
+            splitRanges.forEach(range -> {
+                int newShardId = range.getShardId();
+                routingTable.add(new ArrayList<>(0));
+                routingTable.get(shardId).add(newShardId);
+                docs.add(0);
+                shardRanges.put(newShardId, new SplitShardRange(newShardId, primaryShardId, range));
+            });
+            //recoverDocs(shardId);
+        }
+
+        void routeDoc(Document doc, boolean murmur) {
+            final int routing = doc.getDocId();
+            int shardId = route(routing, murmur);
+        }
+
+        private int route(int routing, boolean murmur) {
+            final int hash;
+            if (murmur) {
+                hash = Murmur3HashFunction.hash("" + routing);
+            } else {
+                hash = routing;
+            }
+
+            int shardId = Math.floorMod(hash, this.routingShard) / this.routingFactor;
+            TreeSet<HashRange> ranges = primaryShardRanges.get(shardId);
+            int rangehash = Math.floorMod(hash, Integer.MAX_VALUE);
+            HashRange range = ranges.floor(new HashRange(rangehash, shardId));
+
+            assert (range.contains(rangehash));
+            shardId = range.getShardId();
+            return shardId;
+        }
+
+        void recoverDocs(final int shardId) {
+
+            // iterate on inMemoryFileStore documents for shardId
+            if(inMemoryFileStore.containsKey(shardId)) {
+                List<Document> docs = inMemoryFileStore.get(shardId);
+                for(Document doc : docs) {
+                    routeDoc(doc, true);
+                }
+            }
+            inMemoryFileStore.remove(shardId);
+            //docs.set(shardId, 0);
+        }
+
+        Document getDoc(int docId) {
+            final int shardId = route(docId, true);
+            assert(inMemoryFileStore.containsKey(shardId));
+            return inMemoryFileStore.get(shardId).stream().filter(doc -> doc.getDocId() == docId).findFirst().get();
+        }
+
+        public static class SplitShardRange {
+            private final int shardId;
+            private final int primaryShardId;
+            private final HashRange range;
+
+            SplitShardRange(int shardId, int primaryShardId, HashRange range) {
+                this.shardId = shardId;
+                this.primaryShardId = primaryShardId;
+                this.range = range;
+            }
+
+            SplitShardRange(int shardId, int primaryShardId) {
+                this.shardId = shardId;
+                this.primaryShardId = primaryShardId;
+                this.range = new HashRange(0, shardId);
+            }
+        }
+
+        public static class HashRange implements Comparable<HashRange> {
+            private final int min;
+            private final int max;
+
+            private final int shardId;
+            private final int upperBound = Integer.MAX_VALUE;
+
+            HashRange(int min, int max, int shardId) {
+                this.min = min;
+                this.max = max;
+                this.shardId = shardId;
+            }
+
+            HashRange(int min, int shardId) {
+                this.min = min;
+                this.max = upperBound;
+                this.shardId = shardId;
+            }
+
+            boolean contains(int hash) {
+                return hash >= min && hash <= max;
+            }
+
+            List<HashRange> splitRange(int numShards, int maxShardId) {
+                List<HashRange> ranges = new ArrayList<>(numShards);
+                int rangeSize = (max - min) / numShards;
+
+                if(rangeSize <= 0) {
+                    throw new IllegalArgumentException("Cannot split further");
+                }
+                int start = min;
+                for (int i = 0; i < numShards; ++i) {
+                    int end = i == numShards - 1 ? max : start + rangeSize;
+                    ranges.add(new HashRange(start, end, ++maxShardId));
+                    start = end + 1;
+                }
+                return ranges;
+            }
+
+            @Override
+            public int compareTo(HashRange o) {
+                return Integer.compare(min, o.min);
+            }
+
+            public int getShardId() {
+                return shardId;
+            }
+        }
+    }
+
     @State(Scope.Thread)
     public static class IndexState {
         IndexWithRecurringRouting index;
+        IndexWithRangeRouting rangeIndex;
         List<Document>docs;
 
         @Param({"1", "100", "1000", "10000", "100000"})
@@ -228,7 +374,8 @@ public class DocRoutingBenchmark {
         @Setup(Level.Iteration)
         public void setup() {
             index = new IndexWithRecurringRouting(1);
-            recursiveSplit(index, 0, depth);
+            rangeIndex = new IndexWithRangeRouting(1);
+            recursiveSplit(index, rangeIndex, 0, depth);
         }
 
         @Setup(Level.Invocation)
@@ -239,22 +386,20 @@ public class DocRoutingBenchmark {
             }
         }
 
-        public static void recursiveSplit(IndexWithRecurringRouting index, int shardId, int numSplits) {
+        public static void recursiveSplit(IndexWithRecurringRouting index, IndexWithRangeRouting rangeIndex, int shardId, int numSplits) {
             if(numSplits <= 0) {
                 return;
             }
             try {
                 index.split(shardId, 2);
+                rangeIndex.Split(shardId, 2);
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
-//            for(int childIds : index.routingTable.get(shardId)) {
-//                recursiveSplit(index, childIds, numSplits-1);
-//            }
             int[] childIds = index.routingTable[shardId];
             for(int childId : childIds) {
-                recursiveSplit(index, childId, numSplits-1);
+                recursiveSplit(index, rangeIndex, childId, numSplits-1);
             }
         }
     }
@@ -264,9 +409,20 @@ public class DocRoutingBenchmark {
     @Measurement(iterations = 2)
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    public void routeDocs(IndexState state) {
+    public void routeDocsRecurring(IndexState state) {
         for(Document doc : state.docs) {
             state.index.routeDoc(doc);
+        }
+    }
+
+    @Fork(value = 1, warmups = 1)
+    @Warmup(iterations = 1)
+    @Measurement(iterations = 2)
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    public void routeDocsRange(IndexState state) {
+        for(Document doc : state.docs) {
+            state.rangeIndex.routeDoc(doc, true);
         }
     }
 }
